6a6335c59556f077fa965ca905a5c437289102fb
SURFACEPRO9\mitsu <mitsuhiro.seino@gmail.com>
2024-05-09 22:35:46 +0900

    表面的にはbabelを排除

--- babel.config.test.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  presets: [['@babel/preset-env', { modules: false, targets: { node: 'current' } }], '@babel/preset-typescript'],
-  plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-transform-modules-commonjs'],
-};
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest/presets/js-with-babel-esm',
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js', 'cjs', 'mjs'],
@@ -9,15 +8,10 @@ module.exports = {
   },
   transform: {
     // node_modules配下のemsをcjsに変換するために追加
-    '^.+\\.m?js$': ['babel-jest', { configFile: './babel.config.test.js' }],
+    '^.+\\.m?js$': ['esbuild-jest', { sourcemap: true }],
     // 当プロジェクトのtsをjsに変換するために追加
-    '^.+\\.ts$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.json',
-        useESM: true,
-      },
-    ],
+    '^.+\\.ts$': ['esbuild-jest', { sourcemap: true }],
   },
+  // tslibはトランスパイルしない
   transformIgnorePatterns: ['node_modules/(?!(tslib))/'],
 };
