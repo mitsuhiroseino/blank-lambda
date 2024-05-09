701848b7197b9c003816ef5149aee4a725b17cce
SURFACEPRO9\mitsu <mitsuhiro.seino@gmail.com>
2024-05-09 20:50:04 +0900

    OK!

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
     statusCode: 200,
-    body: 'OK?OK!',
+    body: 'OK!',
   };
 }
